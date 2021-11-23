01bdd4fe286409787ad4176230ff317197d1b73f
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2021-11-23 19:34:32 -0300

    Desafio Clase 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    TestComponent,
+    PeliculasComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
