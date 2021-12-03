7be3eede46afb184f4fc8d8f48e1c77e8ab4456f
Flavio Salasevicius <flaviosalasevicius@hotmail.com>
2021-12-02 22:05:44 -0300

    Desafio Clase 6

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
+    ngOnInit(): void {
+    }
+       submitted = false;
+       formulario = new FormGroup({
+           nombre: new FormControl ('', [Validators.required, Validators.minLength(3)]),            
+           email: new FormControl ('', [Validators.required, Validators.email]), 
+           genero: new FormControl ('', [Validators.required, Validators.required]), 
+           edad: new FormControl (), 
+       });
+       get val() { return this.formulario.controls; }
 
-  ngOnInit(): void {
-  }
+    guardar() {
+        this.submitted = true;
+
+        if (this.formulario.invalid) {
+            return;
+            
+        }
+        
+        alert('Mensaje Enviado !')
+
+    }
 
 }
---
+++ src/app/models/formulario.ts
@@ -0,0 +1,9 @@
+export class Formulario {
+    constructor(
+        public nombre?: string,
+        public email?:string,
+        public genero?:string,
+        public edad?:number )
+     {}
+   
+}
